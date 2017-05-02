73870d82afd23dfec6fb8f9fb2a52fb4b3da039a
wiffel <wiffel@tilient.net>
2017-05-02 19:28:43 +0200

    gengen test

--- genlist/main.go
+++ genlist/main.go
@@ -5,6 +5,11 @@ import (
 	. "github.com/tilient/gotests/genlist/list"
 )
 
+//---------------------------------------------------------------------
+// go:generate genny -in=list/list.go -out=list/intlist.go gen "т=int"
+//go:generate gengen github.com/tilient/gotests/genlist/list/list2.go int
+//---------------------------------------------------------------------
+
 func main() {
 	var lst *IntList
 	lst = lst.Add(123).Add(321).Add(777)
