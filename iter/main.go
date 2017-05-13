be1548b38671f2202607d9a4c45b3c00e647b471
wiffel <wiffel@tilient.net>
2017-05-13 11:34:26 +0200

    iter without channels

--- iter/main.go
+++ iter/main.go
@@ -2,33 +2,61 @@ package main
 
 import "fmt"
 
-type seq struct {
-	from int
-	to   int
-}
-
-func expandAll(ch chan string, format string, seqs []seq, args ...interface{}) {
-	if len(seqs) == 0 {
-		ch <- fmt.Sprintf(format, args...)
-	} else {
-		for v := seqs[0].from; v <= seqs[0].to; v++ {
-			expandAll(ch, format, seqs[1:], append(args, v)...)
-		}
-	}
-}
-
-func expand(format string, seqs ...seq) chan string {
-	ch := make(chan string)
-	go func() {
-		expandAll(ch, format, seqs)
-		close(ch)
-	}()
-	return ch
-}
+// -----------------------------------------------------------
 
 func main() {
-	filenames := expand("file_%c_%02d.txt", seq{'d', 'f'}, seq{3, 5})
-	for filename := range filenames {
+	filenames := filenames("file_%c_%02d_%c.txt",
+		intRanges{{'d', 'f'}, {3, 5}, {'x', 'z'}})
+	for _, filename := range filenames {
 		fmt.Println(filename)
 	}
 }
+
+// --- format filenames --------------------------------------
+
+func filenames(format string, ranges intRanges) []string {
+	result := []string{}
+	for _, args := range combinations(ranges) {
+		filename := fmt.Sprintf(format, args.asInterfaceList()...)
+		result = append(result, filename)
+	}
+	return result
+}
+
+// --- ranges ------------------------------------------------
+
+type (
+	intRange  [2]int
+	intRanges []intRange
+	intList   []int
+	intLists  []intList
+)
+
+func combinations(ranges intRanges) intLists {
+	return combine(ranges, intList{})
+}
+
+func combine(ranges intRanges, args intList) intLists {
+	if len(ranges) == 0 {
+		return intLists{args}
+	}
+	lst := intLists{}
+	first := ranges[0]
+	rest := ranges[1:]
+	for v := first[0]; v <= first[1]; v++ {
+		lst = append(lst, combine(rest, append(args, v))...)
+	}
+	return lst
+}
+
+// --- tools -------------------------------------------------
+
+func (iLst intList) asInterfaceList() []interface{} {
+	lst := make([]interface{}, len(iLst))
+	for ix, i := range iLst {
+		lst[ix] = i
+	}
+	return lst
+}
+
+// -----------------------------------------------------------
