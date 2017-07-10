923999d8f2b384283de79403ce3e119e2693faaf
wiffel <wiffel@tilient.net>
2017-07-10 10:00:28 +0200

    python and exif

--- exif/main.go
+++ exif/main.go
@@ -4,14 +4,63 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/rwcarlsen/goexif/exif"
 )
 
 func main() {
-	//fname := "SAMPLE.JPG"
-	//fname := "SAMPLE.CR2"
-	fname := "SAMPLE.DNG"
+	dirName := "/mnt/c/wiffel/public/photoKashbah"
+	filepath.Walk(dirName, visit)
+}
+
+func visit(path string, f os.FileInfo, err error) error {
+	if isRawPicture(path) {
+		exists := companionJpeg(path)
+		if exists {
+			fmt.Println("delete", path)
+			os.Remove(path)
+		}
+	}
+	return nil
+}
+
+func isRawPicture(path string) bool {
+	rawExtensions := map[string]bool{
+		".CR2": true,
+		".cr2": true,
+		".dng": true,
+		".DNG": true,
+	}
+	ext := filepath.Ext(path)
+	return rawExtensions[ext]
+}
+
+func companionJpeg(path string) bool {
+	jpgExtensions := []string{".jpg", ".JPG", ".jpeg", ".JPEG"}
+	basename := fileBasename(path)
+	for _, ext := range jpgExtensions {
+		companionFilename := basename + ext
+		if fileExists(companionFilename) {
+			return true
+		}
+	}
+	return false
+}
+
+func fileBasename(path string) string {
+	ext := filepath.Ext(path)
+	return strings.TrimSuffix(path, ext)
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func exifStuff() {
+	fname := "xxx"
 	f, err := os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
@@ -21,5 +70,5 @@ func main() {
 		log.Fatal(err)
 	}
 	tm, _ := x.DateTime()
-	fmt.Println("Taken: ", tm)
+	fmt.Println(fname, "@", tm)
 }
---
+++ python/sum.go
@@ -0,0 +1,23 @@
+package main
+
+// --------------------------------------------------------------
+// go build -buildmode=c-shared -o foo.so
+// python3 -c 'import foo; print(foo.sum(2, 40))'
+// --------------------------------------------------------------
+
+// #cgo pkg-config: python3
+// #define Py_LIMITED_API
+// #include <Python.h>
+// int PyArg_ParseTuple_LL(PyObject *, long long *, long long *);
+import "C"
+
+//export sum
+func sum(self, args *C.PyObject) *C.PyObject {
+	var a, b C.longlong
+	if C.PyArg_ParseTuple_LL(args, &a, &b) == 0 {
+		return nil
+	}
+	return C.PyLong_FromLongLong(a + b)
+}
+
+func main() {}
