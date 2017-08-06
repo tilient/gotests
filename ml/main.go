202d2640b73d7dce89c52a114fa8ccf6feaf8af1
wiffel <wiffel@tilient.net>
2017-08-06 18:18:18 +0200

    ml updates

--- ml/main.go
+++ ml/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"gonum.org/v1/gonum/mat"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -13,13 +14,12 @@ func main() {
 }
 
 func testFun(a, b, c, d float64) float64 {
-	return a + 2*b + c + 3*d
+	return math.Cos(a) + 2*b*c + math.Sin(d)
 }
 
 func test03() {
-	// try to solve X for A * X = Y given A and Y
-	n := 512
-	m := 512
+	n := 128
+	m := 4 * 128
 
 	A := mat.NewDense(n, m, nil)
 	for i := 0; i < n; i++ {
@@ -31,7 +31,8 @@ func test03() {
 	for i := 0; i < n; i++ {
 		//y[i] = 2.0*rand.Float64() - 1.0
 		y[i] = testFun(
-			A.At(i, 0), A.At(i, 1), A.At(i, 2), A.At(i, 3))
+			A.At(i, 0), A.At(i, 1),
+			A.At(i, 2), A.At(i, 3))
 	}
 	Y := mat.NewVecDense(n, y)
 
@@ -47,7 +48,12 @@ func test03() {
 	}
 
 	Yp.MulVec(A, X)
-	fmt.Println("y: ", y[:4])
-	fmt.Println("yp:", yp[:4])
-	fmt.Println("x: ", x[:4])
+	fmt.Println("x: ", x[:3])
+	fmt.Println("y: ", y[:3])
+	fmt.Println("yp:", yp[:3])
+	delta := 0.1
+	for mat.EqualApprox(Y, Yp, delta) {
+		delta /= 10.0
+	}
+	fmt.Println("delta:", 10.0*delta)
 }
